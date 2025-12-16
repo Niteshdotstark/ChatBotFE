1a37c835b25b6437f1f5b38d3287f8f8299c7e99
Nitesh <Nitesh@dotstark.com>
2025-12-16 12:42:23 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // any existing options here
 };
 
 export default nextConfig;
